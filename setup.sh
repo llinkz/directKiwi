cb7d4ee34e61a4e824ecf53842d7ec9cdc197420
llinkz <ounaid@gmail.com>
2019-02-12 00:34:26 +0100

    v4.00 release

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 python -m pip install numpy pillow requests pygame scipy
+cd directKiwi
 echo -e "The setup is now finished.\nTo start the software from console, type ./directKiwi.py"
 sleep 5
